da22acf186e2b2b8bca94aabbceea8f61220f592
math <master_of_puppets57@hotmail.fr>
2017-09-11 10:52:05 +0200

    maj

--- MVC3/vue/blog/header.php
+++ MVC3/vue/blog/header.php
@@ -5,3 +5,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
         <link href="vue/blog/style.css" rel="stylesheet" type="text/css" /> 
     </head>
+        
+    <body>
+        <h1><?php echo $title ?></h1>
